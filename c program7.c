b6e8413bac9a9b06b182464aeab2a6c81ee8ccbe
4bhishekk <138125191+4bhishekk@users.noreply.github.com>
2023-06-29 18:04:07 +0530

    Add files via upload

---
+++ c program7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,x;
+    printf("enter the first value : ");
+    scanf("%d",&num1);
+    printf("enter the second value :");
+    scanf("%d",&num2);
+    x=num1+num2;
+    printf("x=%d",x);
+
+    return 0;
+}
+
